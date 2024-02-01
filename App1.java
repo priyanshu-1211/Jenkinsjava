b65e08da2a07aff37d1b0023b32289fc9a6de312
priyanshu-1211 <priyanshurathore121110@gmail.com>
2024-02-01 16:44:56 +0530

    Commited java

--- App1.java
+++ App1.java
@@ -5,7 +5,7 @@ public class App1{
     public static void main(String[] args){
 
         System.out.println("Java App Pulled & Build by Jenkins");
-        System.out.println("Changes");
+        System.out.println("Changes New");
     }
 
 }
